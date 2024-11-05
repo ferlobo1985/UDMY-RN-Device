3420acde5006a6cb08d73795bc353e094716cb36
Coding rev <fernando.lobo.prez@gmail.com>
2024-11-05 00:20:40 -0300

    09-Working with fonts part 2
    09-Working with fonts part 2

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
 
 /// VIEWS
 // import BatteryComp from './components/battery';
@@ -8,12 +9,21 @@ import { useFonts } from 'expo-font';
 // import FormElements from './components/formElements';
 // import GradientComp from './components/gradient';
 import FontsComp from './components/font';
+import { useEffect } from 'react';
+
+SplashScreen.preventAutoHideAsync()
 
 export default function App() {
-  useFonts({
+  const [loaded,error] = useFonts({
     'imperial':require('./assets/fonts/ImperialScript-Regular.ttf')
-  })
+  });
 
+  useEffect(()=>{
+    if(loaded || error){
+      SplashScreen.hideAsync();
+    }
+  },[loaded,error]);
+  if(!loaded && !error) { return null }
 
   return (
     <View style={styles.container}>
