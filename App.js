b5cf908cabdafbe4c5161023b5534f83f0c5225f
Coding rev <fernando.lobo.prez@gmail.com>
2024-11-05 00:01:02 -0300

    08-Working with fonts part 1
    08-Working with fonts part 1

--- App.js
+++ App.js
@@ -1,16 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
 
 /// VIEWS
 // import BatteryComp from './components/battery';
 // import DeviceComp from './components/device';
 // import FormElements from './components/formElements';
-import GradientComp from './components/gradient';
+// import GradientComp from './components/gradient';
+import FontsComp from './components/font';
 
 export default function App() {
+  useFonts({
+    'imperial':require('./assets/fonts/ImperialScript-Regular.ttf')
+  })
+
+
   return (
     <View style={styles.container}>
-        <GradientComp/>
+        <FontsComp/>
     </View>
   );
 }
